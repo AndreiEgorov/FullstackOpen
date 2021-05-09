836816020a864ae8211bf2b9a1959868d0a85055
AndreiEgorov <andreyegorov1992@gmail.com>
2021-05-09 15:18:36 -0700

    playground

--- playground_not_assignments/src/App.js
+++ playground_not_assignments/src/App.js
@@ -1,35 +1,87 @@
 import React, { useState } from "react";
 
-const Display = (props) => {
-  console.log("Display");
-  return <div>{props.counter}</div>;
-};
+// const Display = (props) => {
+//   console.log("Display");
+//   return <div>{props.counter}</div>;
+// };
 
-const Test = () => {
-  console.log("test");
-  return <div>i am test</div>;
-};
+// const Test = () => {
+//   console.log("test");
+//   return <div>i am test</div>;
+// };
 
-const Button = ({ handler, name }) => {
-  console.log("Display", name);
+// const Button = ({ handler, name }) => {
+//   console.log("Display", name);
 
-  return <button onClick={handler}> {name}</button>;
-};
+//   return <button onClick={handler}> {name}</button>;
+// };
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const setToZero = () => setCounter(0);
+//   const increaseByOne = () => setCounter(counter + 1);
+//   const setToZero = () => setCounter(0);
+//   const mouseMove = (e) => {
+//     console.log("EVENT", e)
+//   }
 
+//   return (
+//     <div onMouseMove={(e)=>mouseMove(e)} style={{background: "orange"}}>
+//       <Display counter={counter} />
+//       <Test />
+//       <Button handler={increaseByOne} name="plus" />
+//       <Button handler={setToZero} name="zero" />
+//     </div>
+//   );
+// };
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <Display counter={counter} />
-      <Test />
-      <Button handler={increaseByOne} name="plus" />
-      <Button handler={setToZero} name="zero" />
+      button press history: {props.allClicks.join(' ')}
     </div>
-  );
-};
+  )
+}
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll([...allClicks, 'R'])
+    setRight(right + 1)
+  }
+
+  const setToNumber = (num) => () => setLeft(num)
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      <Button onClick={setToNumber(1000)} text='to 1000' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
 
 export default App;
